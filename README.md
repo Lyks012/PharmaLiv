e24344578dfa259b94706d38ccb7ae11ca264016
Lyks012 <>
2023-04-18 17:24:26 +0000

    Update from https://github.com/Lyks012/merge-sheets/commit/2ff358808cadf1c6e98d9b7f7dedf6ec6d31c6a8

--- README.md
+++
@@ -1,6 +0,0 @@
-# PharmaLiv
-
-Schema actuel de la base de donnees : 
-![Screenshot from 2021-09-11 01-49-18](https://user-images.githubusercontent.com/54151199/132932382-0b0e9698-758e-4671-ab48-7870d1f8495c.png)
-
-
--- src/view/client/home.php
+++
@@ -1,7 +0,0 @@
-<?php ob_start(); ?>
-<?php require_once('src/view/client/navbarClient.php'); ?>
-<h1>Bienvenue <?= $_SESSION['login'] ?></h1>
-<?php
-$content = ob_get_clean();
-require_once('src/view/shared/template.php')
-?>
